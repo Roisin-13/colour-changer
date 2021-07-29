7a1b599c787d753b404fbc265f1493a1f90c807b
Roisin-13 <86969519+Roisin-13@users.noreply.github.com>
2021-07-29 19:56:22 +0100

    html added, js updated for event listener

--- colour(rgb).js
+++ colour(rgb).js
@@ -1,3 +1,7 @@
+let page = document.getElementById("bodyodyody");
+let text = document.getElementById("clr");
+let button = document.querySelector(".btn");
+
 
 function randNum() {
     return Math.round(Math.random() * 255);
@@ -5,7 +9,15 @@ function randNum() {
 randNum();
 
 function getColour() {
-    let rgb = "randNum(), randNum(), randNum()";
+    let rgb = "rgb (" + randNum() + "," + randNum() + "," + randNum() + ")";
     return rgb;
 };
 console.log(getColour());
+
+
+function colourChange() {
+    button.addEventListener("click", function(){
+        page.style.backgroundColor = rgb;
+        text.innerHTML = `${rgb}`;
+    });
+};
